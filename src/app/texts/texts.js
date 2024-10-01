12bdb40d322a87ccd5370b582310e285b75c3b0a
kevinpicon01 <digitalpruu@gmail.com>
2024-10-01 14:44:22 -0500

    Changed media, color, texts

--- src/app/texts/texts.js
+++ src/app/texts/texts.js
@@ -1,9 +1,9 @@
-const title = "Titulo del emprendimiento";
-const email = "Email del emprendimiento";
+const title = "Juldan Variedades";
+const email = "saneligue08@gmail.com";
 const mensajeWhatsApp = encodeURIComponent(
-    `¡Hola! Estoy muy interesado en tus productos de ${title} y me gustaría recibir más información sobre ellos. Espero poder hablar pronto contigo para aclarar algunas dudas. ¡Gracias!`
+    `¡Hola! Estoy interesado en los productos de ${title} y me gustaría recibir más información. ¡Espero su respuesta!`
 );
-const numeroWhatsApp = "Numero del emprendimiento";
+const numeroWhatsApp = "3004740237";
 const textos = {
     index: {
         title: title,
@@ -12,25 +12,24 @@ const textos = {
         title: title,
     },
     about: {
-        description1: "Descripción del emprendimiento parrafo uno",
-        description2: "Descripcion del emprendimiento parrafo dos"
+        description1: "*Juldan Variedades* se especializa en ofrecer detalles únicos para cumpleaños, desayunos y anchetas para diferentes ocasiones. Cada uno de nuestros productos está pensado para sorprender a esa persona especial.",
+        description2: "No esperes una fecha especial para demostrar tu cariño. En *Juldan Variedades*, te ofrecemos una amplia gama de detalles que se adaptan a cualquier ocasión, asegurándonos de que cada regalo sea memorable.",
     },
     contact: {
         whatsappLink: `https://wa.me/${numeroWhatsApp}?text=${mensajeWhatsApp}`,
-        email: email
+        email: email,
     },
     footer: {
-        slogan:{
-            p1: "slogan del emprendimiento parrafo uno",
-            p2: "slogan del emprendimiento parrafo dos"
+        slogan: {
+            p1: "Juldan Variedades",
+            p2: "No esperes una fecha especial para sorprender a tu persona favorita.",
         },
-        address: "Dirección del emprendimiento",
+        address: "",
         phone: numeroWhatsApp,
         email: email,
-        facebook: "https://www.facebook.com/",
-        instagram: "https://www.instagram.com/",
-        whatsappLink: `https://wa.me/${numeroWhatsApp}?text=${mensajeWhatsApp}`
+        facebook: "https://www.facebook.com/share/sv13WbUF8CNYdPmH/?mibextid=qi2Omg",
+        instagram: "https://www.instagram.com/variedadesjuldan/?utm_source=qr&igsh=eHg2Y2FldXg5eXUw",
+        whatsappLink: `https://wa.me/${numeroWhatsApp}?text=${mensajeWhatsApp}`,
     },
-
 };
 export default textos;
